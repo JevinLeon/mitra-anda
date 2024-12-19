7097f7e0682801f06aaa69a8b479f070ba166943
JevinLeon <jevinleon@gmail.com>
2024-12-19 18:15:57 +0700

    Compressed image + analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Mitra Anda | Batam",
@@ -20,6 +21,7 @@ export default function RootLayout({
         <Navbar />
         <main className="relative overflow-hidden">{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
